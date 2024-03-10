540bf80b37637c1b56222c71b7502b4c41cdf207
Max Page <maxkpage@gmail.com>
2024-03-10 07:38:51 -0500

    Add undocumented search value type

--- lib/dropbox_api/metadata/search_match_type_v2.rb
+++ lib/dropbox_api/metadata/search_match_type_v2.rb
@@ -2,10 +2,12 @@
 
 module DropboxApi::Metadata
   class SearchMatchTypeV2 < DropboxApi::Metadata::Tag
+    # both_filename_and_content is undocumented, but it's returned by the API.
     VALID_VALUES = %i[
       filename
       file_content
       filename_and_content
+      both_filename_and_content
       image_content
     ].freeze
 
